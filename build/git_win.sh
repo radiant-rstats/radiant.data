72929f120c4aecaf66bf80e8d8ade1a0f0433171
vnijs <radiant@rady.ucsd.edu>
2016-06-11 19:45:35 -0700

    cleanup

--- build/git_win.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# run from git terminal
-cd ~/Desktop/GitHub/radiant.data
-git pull
-cd ../DT
-git pull
--- build/update_all.R
+++ build/update_all.R
@@ -12,22 +12,22 @@ system("git push")
 # devtools::install_github("rstudio/DT")
 
 # devtools::install_github("rstudio/shinyapps")
-library(shinyapps)
-fpath <- "~/gh/radiant.data/inst/app"
-setwd(fpath)
-
-for (file in list.files("../../../shinyapps/R", pattern = "\\.(r|R)$", full.names = TRUE))
-  source(file, local = TRUE)
-
-source("../../build/deployapp.R", local = TRUE)
-
-deployApp(account = "vnijs", launch.browser = FALSE, lint = FALSE)
+# library(shinyapps)
+# fpath <- "~/gh/radiant.data/inst/app"
+# setwd(fpath)
+#
+# for (file in list.files("../../../shinyapps/R", pattern = "\\.(r|R)$", full.names = TRUE))
+#   source(file, local = TRUE)
+#
+# source("../../build/deployapp.R", local = TRUE)
+#
+# deployApp(account = "vnijs", launch.browser = FALSE, lint = FALSE)
 
 setwd("~/gh/radiant.data/")
 system("sh build/build_mac_win.sh")
 
 ## in case of problems
-shinyapps::showLogs(entries=1000)
+# shinyapps::showLogs(entries=1000)
 
 ## for major pull problems
 # git fetch --all
