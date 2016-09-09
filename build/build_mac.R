77f2ff4a2c5d08043b3815f5f2a73875630685dc
vnijs <radiant@rady.ucsd.edu>
2016-09-08 21:27:20 -0700

    remove addin for radiant.data

--- build/build_mac.R
+++ build/build_mac.R
@@ -1,4 +1,13 @@
 ## build for mac
-devtools::install("~/gh/radiant.data")
-devtools::build("~/gh/radiant.data")
-devtools::build("~/gh/radiant.data", binary = TRUE)
+# devtools::install("~/gh/radiant.data")
+# devtools::build("~/gh/radiant.data")
+# devtools::build("~/gh/radiant.data", binary = TRUE)
+
+## build for mac
+app <- "radiant.data"
+path <- "~/gh"
+devtools::install(file.path(path, app))
+f <- devtools::build(file.path(path, app))
+curr <- getwd(); setwd(path)
+system(paste0("R CMD INSTALL --build ", f))
+setwd(curr)
--- build/build_win.R
+++ build/build_win.R
@@ -1,3 +1,13 @@
 ## build for windows
-devtools::install("../radiant.data")
-devtools::build("../radiant.data", binary = TRUE)
+# devtools::install("../radiant.data")
+# devtools::build("../radiant.data", binary = TRUE)
+
+## build for windows
+app <- "radiant.data"
+path <- "../"
+devtools::install(file.path(path, app))
+f <- devtools::build(file.path(path, app))
+curr <- getwd(); setwd(path)
+system(paste0("R CMD INSTALL --build ", f))
+setwd(curr)
+
