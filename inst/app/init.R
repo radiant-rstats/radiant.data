3ec34d018b3d17a6e27c5454b51066cef266eb26
vnijs <radiant@rady.ucsd.edu>
2017-12-20 14:01:39 -0800

    styler

--- inst/app/init.R
+++ inst/app/init.R
@@ -14,12 +14,16 @@
 # options(warn=-1)
 
 remove_session_files <- function(st = Sys.time()) {
-  fl <- list.files(normalizePath("~/radiant.sessions/"), pattern = "*.rds",
-                   full.names = TRUE)
+  fl <- list.files(
+    normalizePath("~/radiant.sessions/"),
+    pattern = "*.rds",
+    full.names = TRUE
+  )
 
   for (f in fl) {
-    if (difftime(st, file.mtime(f), units = "days") > 7)
+    if (difftime(st, file.mtime(f), units = "days") > 7) {
       unlink(f, force = TRUE)
+    }
   }
 }
 
@@ -31,13 +35,20 @@ isolate({
 })
 
 most_recent_session_file <- function() {
-  fl <- list.files(normalizePath("~/radiant.sessions/"), pattern = "*.rds",
-                   full.names = TRUE)
+  fl <- list.files(
+    normalizePath("~/radiant.sessions/"),
+    pattern = "*.rds",
+    full.names = TRUE
+  )
 
   if (length(fl) > 0) {
-    data.frame(fn = fl, dt = file.mtime(fl)) %>% arrange(desc(dt)) %>%
-    slice(1) %>% .[["fn"]] %>% as.character %>% basename %>%
-    gsub("r_(.*).rds","\\1",.)
+    data.frame(fn = fl, dt = file.mtime(fl)) %>%
+      arrange(desc(dt)) %>%
+      slice(1) %>%
+      .[["fn"]] %>%
+      as.character() %>%
+      basename() %>%
+      gsub("r_(.*).rds", "\\1", .)
   } else {
     NULL
   }
@@ -48,13 +59,13 @@ r_ssuid <-
   if (getOption("radiant.local")) {
     if (is.null(prevSSUID)) {
       mrsf <- most_recent_session_file()
-      paste0("local-",shiny:::createUniqueId(3))
+      paste0("local-", shiny:::createUniqueId(3))
     } else {
       mrsf <- "0000"
       prevSSUID
     }
   } else {
-    ifelse (is.null(prevSSUID), shiny:::createUniqueId(5), prevSSUID)
+    ifelse(is.null(prevSSUID), shiny:::createUniqueId(5), prevSSUID)
   }
 
 ## (re)start the session and push the id into the url
@@ -62,30 +73,32 @@ session$sendCustomMessage("session_start", r_ssuid)
 
 ## load for previous state if available but look in global memory first
 if (exists("r_data")) {
-  r_data  <- do.call(reactiveValues, r_data)
+  r_data <- do.call(reactiveValues, r_data)
   r_state <- if (exists("r_state")) r_state else list()
   suppressWarnings(rm(r_data, r_state, envir = .GlobalEnv))
 } else if (!is.null(r_sessions[[r_ssuid]]$r_data)) {
-  r_data  <- do.call(reactiveValues, r_sessions[[r_ssuid]]$r_data)
+  r_data <- do.call(reactiveValues, r_sessions[[r_ssuid]]$r_data)
   r_state <- r_sessions[[r_ssuid]]$r_state
 } else if (file.exists(paste0("~/radiant.sessions/r_", r_ssuid, ".rds"))) {
   ## read from file if not in global
-  fn <- paste0(normalizePath("~/radiant.sessions"),"/r_", r_ssuid, ".rds")
+  fn <- paste0(normalizePath("~/radiant.sessions"), "/r_", r_ssuid, ".rds")
 
   rs <- try(readRDS(fn), silent = TRUE)
-  if (is(rs, 'try-error')) {
-    r_data  <- init_data()
+  if (is(rs, "try-error")) {
+    r_data <- init_data()
     r_state <- list()
   } else {
-    if (length(rs$r_data) == 0)
-      r_data  <- init_data()
-    else
-      r_data  <- do.call(reactiveValues, rs$r_data)
+    if (length(rs$r_data) == 0) {
+      r_data <- init_data()
+    } else {
+      r_data <- do.call(reactiveValues, rs$r_data)
+    }
 
-    if (length(rs$r_state) == 0)
+    if (length(rs$r_state) == 0) {
       r_state <- list()
-    else
+    } else {
       r_state <- rs$r_state
+    }
   }
 
   unlink(fn, force = TRUE)
@@ -93,13 +106,13 @@ if (exists("r_data")) {
 } else if (isTRUE(getOption("radiant.local")) && file.exists(paste0("~/radiant.sessions/r_", mrsf, ".rds"))) {
 
   ## restore from local folder but assign new ssuid
-  fn <- paste0(normalizePath("~/radiant.sessions"),"/r_", mrsf, ".rds")
+  fn <- paste0(normalizePath("~/radiant.sessions"), "/r_", mrsf, ".rds")
   rs <- try(readRDS(fn), silent = TRUE)
-  if (is(rs, 'try-error')) {
-    r_data  <- init_data()
+  if (is(rs, "try-error")) {
+    r_data <- init_data()
     r_state <- list()
   } else {
-    r_data  <- if (length(rs$r_data) == 0) init_data() else do.call(reactiveValues, rs$r_data)
+    r_data <- if (length(rs$r_data) == 0) init_data() else do.call(reactiveValues, rs$r_data)
     r_state <- if (length(rs$r_state) == 0) list() else rs$r_state
   }
 
@@ -109,7 +122,7 @@ if (exists("r_data")) {
   unlink(fn, force = TRUE)
   rm(rs)
 } else {
-  r_data  <- init_data()
+  r_data <- init_data()
   r_state <- list()
 }
 
@@ -138,17 +151,19 @@ observeEvent(session$clientData$url_search, {
     ## move through the url
     for (u in names(url)) {
       if (is.null(input[[u]])) return()
-      if (input[[u]] != url[[u]])
+      if (input[[u]] != url[[u]]) {
         updateTabsetPanel(session, u, selected = url[[u]])
-      if (names(tail(url,1)) == u) url_observe$suspend()
+      }
+      if (names(tail(url, 1)) == u) url_observe$suspend()
     }
   })
 })
 
 ## keeping track of the main tab we are on
 observeEvent(input$nav_radiant, {
-  if (!input$nav_radiant %in% c("Refresh", "Stop"))
+  if (!input$nav_radiant %in% c("Refresh", "Stop")) {
     r_data$nav_radiant <- input$nav_radiant
+  }
 })
 
 ## Jump to the page you were on
@@ -156,7 +171,7 @@ observeEvent(input$nav_radiant, {
 if (!is.null(r_state$nav_radiant)) {
 
   ## don't return-to-the-spot if that was quit or stop
-  if (r_state$nav_radiant %in% c("Refresh","Stop")) return()
+  if (r_state$nav_radiant %in% c("Refresh", "Stop")) return()
 
   ## naming the observer so we can suspend it when done
   nav_observe <- observe({
@@ -167,10 +182,11 @@ if (!is.null(r_state$nav_radiant)) {
     ## check if shiny set the main tab to the desired value
     if (is.null(input$nav_radiant)) return()
     if (input$nav_radiant != r_state$nav_radiant) return()
-    nav_radiant_tab <- getOption("radiant.url.list")[[r_state$nav_radiant]] %>% names
+    nav_radiant_tab <- getOption("radiant.url.list")[[r_state$nav_radiant]] %>% names()
 
-    if (!is.null(nav_radiant_tab) && !is.null(r_state[[nav_radiant_tab]]))
+    if (!is.null(nav_radiant_tab) && !is.null(r_state[[nav_radiant_tab]])) {
       updateTabsetPanel(session, nav_radiant_tab, selected = r_state[[nav_radiant_tab]])
+    }
 
     ## once you arrive at the desired tab suspend the observer
     nav_observe$suspend()
@@ -195,7 +211,9 @@ if (!"package:radiant.data" %in% search() && getOption("radiant.path.data") == "
 ## check every 10 seconds if width has been reset
 ## https://github.com/rstudio/rstudio/issues/1870
 ## https://community.rstudio.com/t/rstudio-resets-width-option-when-running-shiny-app-in-viewer/3661
-reactivePoll(10000, session,
+reactivePoll(
+  10000,
+  session,
   checkFunc = function() {
     if (getOption("width", default = 250) != 250) options(width = 250)
   },
--- inst/app/server.R
+++ inst/app/server.R
@@ -1,5 +1,3 @@
-# options(shiny.trace=TRUE)
-
 shinyServer(function(input, output, session) {
 
   ## source shared functions
@@ -10,8 +8,9 @@ shinyServer(function(input, output, session) {
   options(radiant.example.data = "radiant.data")
 
   ## source data & analysis tools
-  for (file in list.files(c("tools/app","tools/data"), pattern="\\.(r|R)$", full.names = TRUE))
+  for (file in list.files(c("tools/app", "tools/data"), pattern = "\\.(r|R)$", full.names = TRUE)) {
     source(file, encoding = getOption("radiant.encoding"), local = TRUE)
+  }
 
   ## save state on refresh or browser close
   saveStateOnRefresh(session)
