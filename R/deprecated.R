49f033819f340bd9006efa077e1e4fd70261896c
vnijs <radiant@rady.ucsd.edu>
2018-05-05 09:25:18 -0700

    pkgdown

--- R/deprecated.R
+++ R/deprecated.R
@@ -6,17 +6,17 @@
 #' @name radiant.data-deprecated
 #' @param ... Parameters to be passed to the updated functions
 #' @docType package
-#' @export  mean_rm
-#' @aliases mean_rmn
+#' @export  mean_rm median_rm min_rm max_rm sd_rm var_rm sum_rm
+#' @aliases mean_rm median_rm min_rm max_rm sd_rm var_rm sum_rm
 #' @section Details:
-#' \tabular{rl}{
-#'   Replace \code{mean_rm} by \code{\link{mean}}\cr
-#'   Replace \code{median_rm} by \code{\link{median}}\cr
-#'   Replace \code{min_rm} by \code{\link{min}}\cr
-#'   Replace \code{max_rm} by \code{\link{max}}\cr
-#'   Replace \code{sd_rm} by \code{\link{sd}}\cr
-#'   Replace \code{var_rm} by \code{\link{var}}\cr
-#'   Replace \code{sum_rm} by \code{\link{sum}}\cr
+#' \itemize{
+#'   \item Replace \code{mean_rm} by \code{\link{mean}}
+#'   \item Replace \code{median_rm} by \code{\link{median}}
+#'   \item Replace \code{min_rm} by \code{\link{min}}
+#'   \item Replace \code{max_rm} by \code{\link{max}}
+#'   \item Replace \code{sd_rm} by \code{\link{sd}}
+#'   \item Replace \code{var_rm} by \code{\link{var}}
+#'   \item Replace \code{sum_rm} by \code{\link{sum}}
 #' }
 #'
 mean_rm <- function(...) {
