96a024344bcd1c8df51ee60d8fa7274f9aeaf41e
vnijs <radiant@rady.ucsd.edu>
2018-05-24 21:43:17 -0700

    cleanup

--- build/build_radiant.R
+++ build/build_radiant.R
@@ -1,25 +1,25 @@
-## could this ensure inst/rstudio/*.dcf are ignored during build?
+d## could this ensure inst/rstudio/*.dcf are ignored during build?
 ## see https://stackoverflow.com/a/42907049/1974918
 # devtools::use_build_ignore(c("inst/rstudio"))
+curr <- getwd()
+pkg <- basename(curr)
 
-## building radiant packages for mac and windows
-setwd("~/gh/")
-
+## building shinyAce packages for mac and windows
 rv <- R.Version()
 rv <- paste0(rv$major, ".", strsplit(rv$minor, ".", fixed = TRUE)[[1]][1])
 
 rvprompt <- readline(prompt = paste0("Running for R version: ", rv, ". Is that what you wanted y/n: "))
 if (grepl("[nN]", rvprompt)) stop("Change R-version")
 
-dirsrc <- file.path("minicran/src/contrib")
+dirsrc <- file.path("../minicran/src/contrib")
 
 if (rv == "3.3") {
-  dirmac <- file.path("minicran/bin/macosx/mavericks/contrib", rv)
+  dirmac <- file.path("../minicran/bin/macosx/mavericks/contrib", rv)
 } else {
-  dirmac <- file.path("minicran/bin/macosx/el-capitan/contrib", rv)
+  dirmac <- file.path("../minicran/bin/macosx/el-capitan/contrib", rv)
 }
 
-dirwin <- file.path("minicran/bin/windows/contrib", rv)
+dirwin <- file.path("../minicran/bin/windows/contrib", rv)
 
 if (!file.exists(dirsrc)) dir.create(dirsrc, recursive = TRUE)
 if (!file.exists(dirmac)) dir.create(dirmac, recursive = TRUE)
@@ -32,31 +32,33 @@ rem_old <- function(app) {
   unlink(paste0(dirwin, "/", app, "*"))
 }
 
-sapply("radiant.data", rem_old)
+sapply(pkg, rem_old)
 
 ## avoid 'loaded namespace' stuff when building for mac
-system(paste0(Sys.which("R"), " -e \"source('~/gh/radiant.data/build/build_mac.R')\""))
+system(paste0(Sys.which("R"), " -e \"setwd('", getwd(), "'); app <- '", pkg, "'; source('build/build_mac.R')\""))
 
 win <- readline(prompt = "Did you build on Windows? y/n: ")
 if (grepl("[yY]", win)) {
 
   ## move packages to radiant_miniCRAN. must package in Windows first
-  setwd("~/gh/")
-  sapply(list.files(".", pattern = "*.tar.gz"), file.copy, dirsrc)
-  unlink("*.tar.gz")
-  sapply(list.files(".", pattern = "*.tgz"), file.copy, dirmac)
-  unlink("*.tgz")
-  sapply(list.files(".", pattern = "*.zip"), file.copy, dirwin)
-  unlink("*.zip")
+  path <- normalizePath("../")
+  sapply(list.files(path, pattern = "*.tar.gz", full.names = TRUE), file.copy, dirsrc)
+  unlink("../*.tar.gz")
+  sapply(list.files(path, pattern = "*.tgz", full.names = TRUE), file.copy, dirmac)
+  unlink("../*.tgz")
+  sapply(list.files(path, pattern = "*.zip", full.names = TRUE), file.copy, dirwin)
+  unlink("../*.zip")
 
   tools::write_PACKAGES(dirmac, type = "mac.binary")
   tools::write_PACKAGES(dirwin, type = "win.binary")
   tools::write_PACKAGES(dirsrc, type = "source")
 
   # commit to repo
-  setwd("~/gh/minicran")
+  setwd("../minicran")
   system("git add --all .")
-  mess <- paste0("radiant package update: ", format(Sys.Date(), format = "%m-%d-%Y"))
+  mess <- paste0(pkg, " package update: ", format(Sys.Date(), format = "%m-%d-%Y"))
   system(paste0("git commit -m '", mess, "'"))
   system("git push")
 }
+
+setwd(curr)
