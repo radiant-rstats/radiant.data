d121574111991e69dbcc3716ae3ec4c610acfe63
vnijs <radiant@rady.ucsd.edu>
2018-08-30 00:31:28 +0000

    build fix

--- build/build_mac.R
+++ build/build_mac.R
@@ -1,6 +1,8 @@
 ## build for mac
+# setwd(rstudioapi::getActiveProject())
+app <- basename(getwd())
 curr <- setwd("../")
-devtools::install(app)
+# devtools::install(app)
 f <- devtools::build(app)
 system(paste0("R CMD INSTALL --build ", f))
 setwd(curr)
