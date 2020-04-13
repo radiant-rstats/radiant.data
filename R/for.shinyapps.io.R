c55d652c154ebb9c80a37628d44cc683e699c2d4
vnijs <radiant@rady.ucsd.edu>
2020-04-13 00:53:13 -0700

    shinyapps.io

---
+++ R/for.shinyapps.io.R
@@ -0,0 +1,29 @@
+## install the latest version from github so it will be used on shinyapps.io
+packages <- "radiant-rstats/radiant.data"
+packages <- c(packages, "trestletech/shinyAce", "thomasp85/shinyFiles")
+
+## Use the code below to install the development version
+if (!require(remotes)) {
+  install.packages("remotes")
+}
+ret <- sapply(
+  packages,
+  function(p) {
+    remotes::install_github(
+      p,
+      dependencies = FALSE,
+      upgrade = "never"
+    )
+  }
+)
+
+install.packages("htmltools", repo = "https://cloud.r-project.org/")
+
+## by listing the call to the radiant library it will get picked up as a dependency
+# library(radiant)
+library(radiant.data)
+library(rstudioapi)
+library(shinyAce)
+library(shinyFiles)
+library(DT)
+library(htmltools)
