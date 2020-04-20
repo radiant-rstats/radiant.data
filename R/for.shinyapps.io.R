f9dbe1d6554b8380b10bdf3093c16525a534e8ad
vnijs <radiant@rady.ucsd.edu>
2020-04-20 16:56:15 -0700

    comment out

--- R/for.shinyapps.io.R
+++ R/for.shinyapps.io.R
@@ -1,8 +1,8 @@
-## install the latest version from github so it will be used on shinyapps.io
-packages <- "radiant-rstats/radiant.data"
-packages <- c(packages, "trestletech/shinyAce", "thomasp85/shinyFiles")
-
-## Use the code below to install the development version
+# ## install the latest version from github so it will be used on shinyapps.io
+# packages <- "radiant-rstats/radiant.data"
+# packages <- c(packages, "trestletech/shinyAce", "thomasp85/shinyFiles")
+#
+# ## Use the code below to install the development version
 # if (!require(remotes)) {
 #   install.packages("remotes")
 # }
@@ -16,10 +16,10 @@ packages <- c(packages, "trestletech/shinyAce", "thomasp85/shinyFiles")
 #     )
 #   }
 # )
-
-# install.packages("htmltools", repo = "https://cloud.r-project.org/")
-
-## by listing the call to the radiant library it will get picked up as a dependency
+#
+# # install.packages("htmltools", repo = "https://cloud.r-project.org/")
+#
+# ## by listing the call to the radiant library it will get picked up as a dependency
 # library(radiant)
 # library(radiant.data)
 # library(rstudioapi)
