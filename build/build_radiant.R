bdf8835e465b1d6ce88be5f087101deac047bd97
vnijs <radiant@rady.ucsd.edu>
2020-05-14 23:20:30 -0700

    CRAN

--- build/build_radiant.R
+++ build/build_radiant.R
@@ -23,8 +23,11 @@ if (grepl("[nN]", rvprompt)) stop("Change R-version")
 
 dirsrc <- "../minicran/src/contrib"
 
+
 if (rv == "3.3") {
   dirmac <- fs::path("../minicran/bin/macosx/mavericks/contrib", rv)
+} else if (as.numeric(substr(rv, 1, 1)) >= 4) {
+  dirmac <- fs::path("../minicran/bin/macosx/contrib", rv)
 } else {
   dirmac <- fs::path("../minicran/bin/macosx/el-capitan/contrib", rv)
 }
