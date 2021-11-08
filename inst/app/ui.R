ad9b794a661740e4c51c10d21449a27ed79fcfb8
vnijs <vnijs@ucsd.edu>
2021-11-08 01:15:35 +0000

    suppressWarnings bslib/shiny warnings

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -1,12 +1,14 @@
 ## ui for data menu in radiant
 navbar_proj(
-  do.call(
-    navbarPage,
-    c(
-      "Radiant",
-      getOption("radiant.nav_ui"),
-      getOption("radiant.shared_ui"),
-      help_menu("help_data_ui")
+  suppressWarnings(
+    do.call(
+      navbarPage,
+      c(
+        "Radiant",
+        getOption("radiant.nav_ui"),
+        getOption("radiant.shared_ui"),
+        help_menu("help_data_ui")
+      )
     )
   )
 )
