c44347483573f616e061ee8c784ec5159b38f2ec
vnijs <vnijs@ucsd.edu>
2021-11-08 02:19:14 +0000

    suppress only in radiant

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -1,14 +1,12 @@
 ## ui for data menu in radiant
 navbar_proj(
-  suppressWarnings(
-    do.call(
-      navbarPage,
-      c(
-        "Radiant",
-        getOption("radiant.nav_ui"),
-        getOption("radiant.shared_ui"),
-        help_menu("help_data_ui")
-      )
+  do.call(
+    navbarPage,
+    c(
+      "Radiant",
+      getOption("radiant.nav_ui"),
+      getOption("radiant.shared_ui"),
+      help_menu("help_data_ui")
     )
   )
 )
